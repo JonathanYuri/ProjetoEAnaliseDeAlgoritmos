3ae4d21aafa529964d1baf2f67999df8286f492d
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2022-07-09 12:29:44 -0300

    Update Quadrado-mágico.c

--- Quadrado-mágico.c
+++ Quadrado-mágico.c
@@ -2,105 +2,69 @@
 
 int somas_dif_coluna(int q[][3], int coluna, int soma)
 {
-    if (q[0][coluna] == 0 || q[1][coluna] == 0 || q[2][coluna] == 0)
+    int soma_coluna = 0;
+    for (int i = 0; i < 3; i++)
     {
-        return 0;
+        if (q[i][coluna] == 0)  return 0;
+        soma_coluna += q[i][coluna];
     }
-    else
-    {
-        if (q[0][coluna] + q[1][coluna] + q[2][coluna] == soma)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+    if (soma_coluna == soma)    return 0;
+    else    return 1;
 }
 
 int somas_dif_linha(int q[][3], int linha, int soma)
 {
-    if (q[linha][0] == 0 || q[linha][1] == 0 || q[linha][2] == 0)
+    int soma_linha = 0;
+    for (int j = 0; j < 3; j++)
     {
-        return 0;
+        if (q[linha][j] == 0)   return 0;
+        soma_linha += q[linha][j];
     }
-    else
-    {
-        if (q[linha][0] + q[linha][1] + q[linha][2] == soma)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+    if (soma_linha == soma) return 0;
+    else    return 1;
 }
 
 int somas_dif_diag1(int q[][3], int soma)
 {
-    if (q[0][0] == 0 || q[1][1] == 0 || q[2][2] == 0)
+    int soma_diag = 0;
+    for (int i = 0; i < 3; i++)
     {
-        return 0;
+        if (q[i][i] == 0)   return 0;
+        soma_diag += q[i][i];
     }
-    else
-    {
-        if (q[0][0] + q[1][1] + q[2][2] == soma)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+    if (soma_diag == soma)  return 0;
+    else    return 1;
 }
 
 int somas_dif_diag2(int q[][3], int soma)
 {
-    if (q[0][2] == 0 || q[1][1] == 0 || q[2][0] == 0)
+    int soma_diag = 0, j = 2;
+    for (int i = 0; i < 3; i++)
     {
-        return 0;
+        if (q[i][j] == 0)   return 0;
+        soma_diag += q[i][j];
+        j--;
     }
-    else
-    {
-        if (q[0][2] + q[1][1] + q[2][0] == soma)
-        {
-            return 0;
-        }
-        else
-        {
-            return 1;
-        }
-    }
+    if (soma_diag == soma)    return 0;
+    else    return 1;
 }
 
-int somas_dif(int q[][3], int pos)
+int somas_dif(int q[][3], int k)
 {
     int soma = q[0][0] + q[0][1] + q[0][2];
-    // 0 1 2    (0,0) (0,1) (0,2)
-    // 3 4 5    (1,0) (1,1) (1,2)
-    // 6 7 8    (2,0) (2,1) (2,2)
+    int coluna = k%3;
+    int linha = k/3;
     
-    int coluna = pos%3; // coluna -> pos % 3
-    int linha = pos/3;
     if (somas_dif_coluna(q, coluna, soma) == 1) return 1;
     if (somas_dif_linha(q, linha, soma) == 1)   return 1;
     
     if (coluna == linha) // pertence a diag principal
     {
-        if (somas_dif_diag1(q, soma) == 1)
-        {
-            return 1;
-        }
+        if (somas_dif_diag1(q, soma) == 1)  return 1;
     }
     if (coluna == 2-linha) // diag secundaria
     {
-        if (somas_dif_diag2(q, soma) == 1)
-        {
-            return 1;
-        }
+        if (somas_dif_diag2(q, soma) == 1)  return 1;
     }
     
     return 0;
@@ -110,55 +74,41 @@ int Usado(int usados[], int x)
 {
     for (int i = 0; i < 9; i++)
     {
-        if (usados[i] == 0)
-        {
-            return 0;
-        }
-        else if (usados[i] == x)
-        {
-            return 1;
-        }
+        if (usados[i] == 0) return 0;
+        else if (usados[i] == x)    return 1;
     }
     return 0;
+}
+
+void ImprimirMatriz(int q[][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%i ", q[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void QM(int q[][3], int k, int usados[])
 {
     if (k == 9)
     {
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                printf("%i ", q[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\n");
+        ImprimirMatriz(q);
         return;
     }
     int l = 1;
     while (l <= 9)
     {
         q[k/3][k%3] = l;
-        if (somas_dif(q, k) == 0) // se nao da ruim a soma
+        // se nao da ruim a soma e se o numero nao foi usado ainda
+        if (somas_dif(q, k) == 0 && Usado(usados, l) == 0)
         {
-            if (Usado(usados, l) == 0) // e se nao foi usado o numero ainda
-            {
-                /* ETAPAS
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        printf("%i ", q[i][j]);
-                    }
-                    printf("\n");
-                }
-                printf("\n");*/
-                
-                usados[k] = l;
-                QM(q, k+1, usados);
-            }
+            usados[k] = l;
+            QM(q, k+1, usados);
         }
         
         // lixo
